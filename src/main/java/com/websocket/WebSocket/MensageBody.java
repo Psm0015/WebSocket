420185398c41c3332c6564a6e9e906a22e2b0fb8
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-05-31 13:58:35 -0300

    primeiro Web Socket Funcional

---
+++ src/main/java/com/websocket/WebSocket/MensageBody.java
@@ -0,0 +1,14 @@
+package com.websocket.WebSocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MensageBody {
+    private String content;
+}
--- src/main/java/com/websocket/WebSocket/WebSocketController.java
+++ src/main/java/com/websocket/WebSocket/WebSocketController.java
@@ -3,13 +3,14 @@ package com.websocket.WebSocket;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class WebSocketController {
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public String greeting(String message) {
-        return "Hello, " + message + "!";
+    public String greeting(@RequestBody MensageBody msg) {
+        return "Hello, " + msg.getContent() + "!";
     }
 }
